186e10ccc219dfee975303065ee6601babb0f63d
popelini <ccastillon92@gmail.com>
2017-11-06 17:47:29 +0100

    Iteration 2 finished

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -17,8 +17,14 @@ class Elevator {
   }
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
+  floorUp() {
+    if(this.floor<this.MAXFLOOR) this.floor+=1;
+    else console.log("You are already on the top floor!");
+  }
+  floorDown() {
+    if(this.floor>0) this.floor-=1;
+    else console.log("Can't dig deeper than floor 0!");
+  }
   call() { }
   log() {
     console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
